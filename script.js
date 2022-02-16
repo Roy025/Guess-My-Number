26af66cb0f84b79a1d01b96679d8226f5f166271
Roy025 <prapti.roy025@gmail.com>
2022-02-17 02:02:08 +0600

    2nd commit

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const num = Math.trunc(Math.random() * 20) + 1;
-let sc = 2;
+let num = Math.trunc(Math.random() * 20) + 1;
+let sc = 20;
+let hsc = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //console.log(typeof guess);
@@ -20,6 +21,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = num;
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '60rem';
+
+    if (sc > hsc) {
+      hsc = sc;
+      document.querySelector('.highscore').textContent = hsc;
+    }
   }
   if (sc > 0) {
     if (guess > num) {
@@ -33,4 +39,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'You lost the game';
   }
   document.querySelector('.score').textContent = sc;
+});
+document.querySelector('.again').addEventListener('click', function () {
+  sc = 20;
+  let num = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = sc;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.number').style.width = '15rem';
 });
